3f30e1712f94df60204a5eb555b92b21795dadac
Nathaniel Driggs <nathanieldriggs@gmail.com>
2019-12-11 17:53:23 -0700

    Adding comments and some editing of the regressions

--- FamaFrench.R
+++ FamaFrench.R
@@ -14,8 +14,7 @@ install.packages("ggplot2")
 install.packages("MASS")
 
 
-## changes - add readr, dplyr, lubridate
-## add L to NUL
+
 ## is the date column of stk removed somewhere along the way?
 
 
@@ -28,11 +27,11 @@ library(tidyverse)
 library(timetk)
 library(broom)
 library(glue)
-library("readr")
-library("dplyr")
-library("lubridate")
-library("ggplot2")
-library("MASS")
+library(readr)
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+library(MASS)
 
 #Define the date range:
 open_date  = "1997-12-31"
@@ -41,19 +40,20 @@ close_date = Sys.Date()
 #Choose ticker:
 ticker_name = "AAPL"
 
-#Load data into the environment:
-
+#Load historical stock data into the environment:
 stk <-data.frame(getSymbols(ticker_name,
                             src='yahoo',
                             reload.Symbols = TRUE,
                             from = open_date,
                             to = close_date,
                             auto.assign = FALSE))
+
+#make the rownames its own "date" column
 stk$date<-as.Date(rownames(stk),format="%Y-%m-%d")
 rownames(stk)<-NULL
 View(stk)
 
-
+#download the Fama French 5 factor data then read from the csv
 temp <- tempfile()
 base <-
   "http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/ftp/"
@@ -84,21 +84,43 @@ FF5 <-
            ymd(parse_date_time(date, "%Y%m%d")))
 View(FF5)
 
+#combine the stock and Fama French data frames
 df <- merge(x=stk, y=FF5, by='date', all = FALSE)
 
 #add an Excess Returns column for returns greater than the risk free rate
 df <- mutate(df, 
              Excess_Returns = ((df$AAPL.Close - df$AAPL.Open) / df$AAPL.Open ) - df$RF)
 
-#run the regression
-colnames(df)[8] <- "Mkt_RF" #rename the column so it's workable
+#rename the column so the functions can work with it
+#just changing the hyphen to an underscore
+colnames(df)[8] <- "Mkt_RF" 
+
+#run the regression without interactions
 no_interactions_fit <- lm(Excess_Returns ~ Mkt_RF + SMB + HML + RMW + CMA, data = df)
+
+#look at the betas
+no_interactions_fit$coefficients
+
+#view the anova table, we see all factors except for SMB are significant at 
+#at least the .05 level. SMB is signicant at the .1 level
+summary(no_interactions_fit)
+
+#use the stepAIC function for variable selection to see if we can 
+#improve our model
 step1 <- stepAIC(no_interactions_fit, direction = "both")
+
+#view the selected model, we see that the final model
+#still has all 5 factors and could not be reduced
 step1$anova             
 
-
+#run the regression with interactions
 interaction_fit <- lm(Excess_Returns ~ (Mkt_RF + SMB + HML + RMW + CMA)^2, data = df)
+
+#use the stepAIC function again for variable selection
 step2 <- stepAIC(interaction_fit, direction = "both")
+
+#view the selected model. We see that only 5 of 10 possible interactions
+#are included in the final model, in addition to the individual 5 factors 
 step2$anova
 
 #we see that the AIC for the interaction fit is lower and is thus a better fit
